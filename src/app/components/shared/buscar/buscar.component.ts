6aa8a03f04d7dac22d25fedd5d10fe1b14bdfd75
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-01-22 21:45:21 -0300

    Agregamos vista de busqueda

--- src/app/components/shared/buscar/buscar.component.ts
+++ src/app/components/shared/buscar/buscar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Heroe, HeroesService } from '../../../services/heroes.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-buscar',
@@ -10,13 +10,17 @@ export class BuscarComponent implements OnInit {
 
   heroesResults: Heroe[] = [];
 
-  constructor( private heroesService: HeroesService, private activatedRoute: ActivatedRoute ) {
+  constructor( private heroesService: HeroesService, private router: Router, private activatedRoute: ActivatedRoute ) {
     this.activatedRoute.params.subscribe( params => {
       this.heroesResults = this.heroesService.buscarHeroes( params['termino'] );
     } );
    }
 
   ngOnInit() {
+  }
+
+  verHeroe( id: number ) {
+    this.router.navigate(['/heroe', id]);
   }
 
 }
